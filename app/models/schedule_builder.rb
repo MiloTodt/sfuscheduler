79cfc9ad47c7dc5f81e3722b48635c8ae18960fe
Milo Todt <milotodt@gmail.com>
2017-06-28 16:09:10 -0700

    added SCD check

--- app/models/schedule_builder.rb
+++ app/models/schedule_builder.rb
@@ -2,7 +2,12 @@ class ScheduleBuilder < ActiveRecord::Base
     has_many :data_base
 
     def index
+        
+    end
+
+    def create
         data = params[:order].split(',')
     end
+    
     
 end
